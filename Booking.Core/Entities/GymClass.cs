f883e82ad3c086a40816bf17e46b201cdbcf8a35
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-01-24 11:36:26 +0100

    Relationship

--- Booking.Core/Entities/GymClass.cs
+++ Booking.Core/Entities/GymClass.cs
@@ -10,9 +10,11 @@ namespace Booking.Core.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
         public TimeSpan Duration { get; set; }
-        public DateTime EndTime => StartDate + Duration;
-        public string Description { get; set; } = String.Empty;
+        public DateTime EndTime => StartTime + Duration;
+        public string Description { get; set; } = string.Empty;
+
+        public ICollection<ApplicationUserGymClass> AttendingMembers { get; set; } = new List<ApplicationUserGymClass>();
     }
 }
