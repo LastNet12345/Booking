f45c33368abcc8318a1b0bc684179a15ff4c3b3e
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-01-24 11:25:53 +0100

    Add project files.

---
+++ Booking.Core/Entities/GymClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booking.Core.Entities
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime => StartDate + Duration;
+        public string Description { get; set; } = String.Empty;
+    }
+}
