ac68526ab5bcb335740ef644d7b18b63011792c2
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2023-01-30 11:55:16 +0100

    ApplicationUserGymClassRepository

---
+++ Booking.Data/Repositories/IApplicationUserGymClassRepository.cs
@@ -0,0 +1,11 @@
+﻿using Booking.Core.Entities;
+
+namespace Booking.Data.Repositories
+{
+    public interface IApplicationUserGymClassRepository
+    {
+        void Add(ApplicationUserGymClass booking);
+        Task<ApplicationUserGymClass?> FindAsync(string userId, int gymClassId);
+        void Remove(ApplicationUserGymClass attending);
+    }
+}
